45e184ccef5cafd91796774dc4898bcbc6faae9f
richardjtelford <richard.telford@bio.uib.no>
2019-10-09 23:24:13 +0200

    cex.yaxis

--- R/secondary_scale.R
+++ R/secondary_scale.R
@@ -9,6 +9,7 @@
 #' @param xLeft Position of plot. See \code{\link[rioja]{strat.plot}}
 #' @param ylabel2 character. Label for secondary scale
 #' @param cex.ylabel2 numeric. Size of axis label.
+#' @param cex.yaxis numeric. Size of axis values.
 #' @importFrom rioja figCnvt
 #' @importFrom graphics axis mtext par plot
 #' @importFrom stats approx
@@ -22,9 +23,9 @@
 #' secondary_scale(pt, yvar = chron$Depth, yvar2 = chron$Age, n = 5, ylabel2 = "Years")
 #' @export
 
-secondary_scale <- function(x, yvar, yvar2, n = 5,
-                            xLeft = 0.11,
-                            ylabel2 = "", cex.ylabel2 = 1){
+secondary_scale <- function(
+  x, yvar, yvar2, n = 5, xLeft = 0.11,
+  ylabel2 = "", cex.ylabel2 = 1, cex.yaxis = 0.8){
   orig.fig <- par("fig")
   agedepth <- approx(yvar2, yvar, xout = pretty(yvar2, n = n))
   par(mai = c(0, 0, 0, 0))
@@ -34,7 +35,8 @@ secondary_scale <- function(x, yvar, yvar2, n = 5,
     )
 
   plot(0, type = "n", axes = FALSE, ann = FALSE, yaxs = "r", ylim = x$ylim)
-  axis(side = 2, at = agedepth$y, labels = agedepth$x, las= 2, xpd = NA)
+  axis(side = 2, at = agedepth$y, labels = agedepth$x,
+       las= 2, xpd = NA,  cex.axis=cex.yaxis)
   mtext(ylabel2, side = 2, line = 2.5, cex = cex.ylabel2)
   par("fig" = orig.fig) #reset "fig"
 }
