990fe98be765b90b8b4fb723915e3b5837b663e0
richardjtelford <richard.telford@bio.uib.no>
2019-12-23 18:11:51 +0100

    better detection of authorities

--- R/species_to_latin.R
+++ R/species_to_latin.R
@@ -9,7 +9,10 @@
 #' @param first_capital_only logical; if TRUE only the first word starting with a capital letter is italicised
 #' @param no_numbers logical; if TRUE any words with numbers are not italicised
 #' @param no_extra_capitals logical; if TRUE any words with capitals other than first letter are not italicised
+#' @param not_after_capital logical; if TRUE, nothing after first extra capital is capitalised
 #' @param text logical; TRUE gives output as character vector; FALSE gives list of expressions
+#' @details Uses various heuristics to identify which parts of the name should be italicised and which should not. Should identify authority (if present) by the presence of the capital letter. Will probably fail with informal names unless included in `etc`.
+#' @return A character vector if ``text` is TRUE, otherwise a list of expressions.
 #' @examples
 #' species_to_italics(c("Navicula spp.", "Navicula spp", "Poaceae"))
 #' library(ggplot2)
@@ -27,7 +30,7 @@
 #'              labeller = as_labeller(
 #'                species_to_italics,
 #'                default = label_parsed))
-#' @importFrom stringr str_split str_detect str_replace_all str_remove_all str_replace
+#' @importFrom stringr str_split str_detect str_replace_all str_remove_all str_replace str_which
 #' @importFrom purrr map map_chr
 #' @importFrom dplyr case_when
 #' @importFrom magrittr %>%
@@ -43,6 +46,7 @@ species_to_italics <- function(
   first_capital_only = TRUE,
   no_numbers = TRUE,
   no_extra_capitals = TRUE,
+  not_after_capital = TRUE,
   text = TRUE){
 
   #postfix
@@ -55,7 +59,8 @@ species_to_italics <- function(
                          first_with_dot_only,
                          first_capital_only,
                          no_numbers,
-                         no_extra_capitals){
+                         no_extra_capitals,
+                         not_after_capital){
 
     #position nr
     n <- seq_along(xx)
@@ -66,15 +71,16 @@ species_to_italics <- function(
 
     #detect extra capitals
     extra_capitals <- str_which(xx, "[A-Z]")
-    extra_capitals <- extra[extra > 1]
+    extra_capitals <- c(extra_capitals[extra_capitals > 1], Inf)[1]
 
     #italicise as required
     xx <- case_when(
       families & str_detect(xx, "(aceae)|(idae)$") ~ xx,
       first_with_dot_only & str_detect(xx, "\\.") & n > 1 ~ xx,
-      first_capital_only & str_detect(xx, "$[A-Z]")  & n > 1 ~ xx,
+      first_capital_only & str_detect(xx, "^[A-Z]")  & n > 1 ~ xx,
       no_numbers & str_detect(xx, "\\d") ~ xx,
       no_extra_capitals & str_detect(xx, "^.+[A-Z]") ~ xx,
+      not_after_capital & n > extra_capitals ~ xx,
       str_detect(xx, postfix) ~ str_replace(xx, postfix, "italic(\\1)\\2"),
       xx %in% c(modifiers, etc) ~ xx,
       TRUE ~ paste0("italic(", xx, ")")
@@ -102,7 +108,8 @@ species_to_italics <- function(
                     first_with_dot_only = first_with_dot_only,
                     first_capital_only = first_capital_only,
                     no_numbers = no_numbers,
-                    no_extra_capitals = no_extra_capitals)
+                    no_extra_capitals = no_extra_capitals,
+                    not_after_capital = not_after_capital)
 
   if(text){
     return(result)
--- tests/testthat/test-species_to_latin.R
+++ tests/testthat/test-species_to_latin.R
@@ -7,3 +7,8 @@ test_that("modifiers ignored", {
   x <- "Navicula elkab agg"
   expect_equal("italic(Navicula)~italic(elkab)~agg", species_to_italics(x, text = TRUE))
 })
+
+test_that("modifiers ignored", {
+  x <- "Navicula elkab O.Müller ex O.Müller"
+  expect_equal("italic(Navicula)~italic(elkab)~O.Müller~ex~O.Müller", species_to_italics(x, text = TRUE))
+})
